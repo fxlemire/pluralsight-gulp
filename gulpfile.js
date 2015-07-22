64e4e81087d037bdecb0c07533b06097da7b80bd
Francois-Xavier Lemire <francois-xavier.lemire@mail.mcgill.ca>
2015-07-22 14:29:59 -0700

    third of course completed: cannot generate bower js and css through wiredep

---
+++ gulpfile.js
@@ -0,0 +1,88 @@
+var args = require('yargs').argv;
+var config = require('./gulp.config')();
+var del = require('del');
+var gulp = require('gulp');
+
+var $ = require('gulp-load-plugins')({lazy: true});
+
+/* Those dependencies are not needed since we use the plugin 'gulp-load-plugins' */
+//var jshint = require('gulp-jshint');
+//var jscs = require('gulp-jscs');
+//var util = require('gulp-util');
+//var gulpprint = require('gulp-print');
+//var gulpif = require('gulp-if');
+
+gulp.task('vet', function() {
+    log('Analyzing source with JSHint and JSCS');
+
+    return gulp
+        .src(config.alljs)
+        .pipe($.if(args.verbose, $.print()))
+        .pipe($.jscs())
+        .pipe($.jshint())
+        .pipe($.jshint.reporter('jshint-stylish', {verbose: true}))
+        .pipe($.jshint.reporter('fail'));
+});
+
+gulp.task('styles', ['clean-styles'], function() {
+    log('Compiling Less --> CSS');
+
+    return gulp
+        .src(config.less)
+        .pipe($.plumber())
+        .pipe($.less())
+        //.on('error', errorLogger) // plumber takes care of this step instead
+        .pipe($.autoprefixer({browsers: ['last 2 version', '> 5%']}))
+        .pipe(gulp.dest(config.temp));
+});
+
+// we want clean-styles to have a callback (done) since it does not use a stream
+gulp.task('clean-styles', function(done) {
+    var files = config.temp + '**/*.css';
+    clean(files, done);
+});
+
+gulp.task('less-watcher', function() {
+    gulp.watch([config.less], ['styles']);
+});
+
+gulp.task('wiredep', function() {
+    var options = config.getWiredepDefaultOptions();
+    var wiredep = require('wiredep').stream;
+
+    return gulp
+        .src(config.index)
+        .pipe(wiredep(options))
+        .pipe($.inject(gulp.src(config.js)))
+        .pipe(gulp.dest(config.client));
+});
+
+/************************
+** FUNCTIONS
+************************/
+
+function clean(path, done) {
+    log('Cleaning: ' + $.util.colors.blue(path));
+    del(path, done);
+}
+
+/*
+function errorLogger(error) {
+    log('*** Start of Error ***');
+    log(error);
+    log('*** End of Error ***');
+    this.emit('end');
+}
+*/
+
+function log(msg) {
+    if (typeof(msg) === 'object') {
+        for (var item in msg) {
+            if (msg.hasOwnProperty(item)) {
+                $.util.log($.util.colors.blue(msg[item]));
+            }
+        }
+    } else {
+        $.util.log($.util.colors.blue(msg));
+    }
+}
